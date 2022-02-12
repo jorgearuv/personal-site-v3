baf853c04fc7d473f0c53007754bec7dd17a6ec7
Jorge Ruvalcaba <jorgeruvalcabav@gmail.com>
2022-02-12 13:14:02 -0600

    fix: updating notion-x packages

--- site.config.js
+++ site.config.js
@@ -25,7 +25,7 @@ module.exports = {
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
-  defaultPageCoverPosition: 0.1862,
+  defaultPageCoverPosition: 0.5,
 
   // image CDN host to proxy all image requests through (optional)
   // NOTE: this requires you to set up an external image proxy
@@ -47,19 +47,5 @@ module.exports = {
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: {
-    '/about': 'da6cd63a22b3429f8627bfb16c11dd69',
-    '/projects': 'eca3100837544fbfae31d0f8efdba27c',
-    '/contact': '2a96bb6228e2436b9d3df8754694fd11',
-    '/add-tab-to-search-in-your-gatsby-blog':
-      '8ca8e557d080439fa484a1541fb358c6',
-    '/welcome-to-my-crib': '25ec99f180b64499a13a4762e2261338',
-    '/gmail-clone': '673819d15c1142f6b69f5c2ba10645f4',
-    '/markdown-previewer': '2d642f52a4b541f8904ebd18e7df8b42',
-    '/personal-site-v2': '02a653ca505840b197dd488efacb1688',
-    '/drum-machine': '2983d4a7e7b24b18b6e13ba538313754',
-    '/random-kanye-west-quote-generator': '13af8adabebd4f4d90c8c00c04a9d3d7',
-    '/uses': 'fff94bbbabb44b9a9d3ca2e77aaf1c37',
-    '/setting-up-namecheap-domain-in-vercel': 'fa12dcf7b5954de2bb2284d05dd579c0'
-  }
+  pageUrlOverrides: null
 }
