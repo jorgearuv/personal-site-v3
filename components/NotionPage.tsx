e73365699850399c42c1006449804d33cdf59917
Jorge Ruvalcaba <jorgeruvalcabav@gmail.com>
2022-02-14 12:35:54 -0600

    fix: adding defaultPageCover

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -241,6 +241,8 @@ export function NotionPage({
     getPageProperty<string>('Description', block, recordMap) ||
     config.description
 
+  console.log('socialImage', socialImage)
+
   return (
     <>
       <PageHead
