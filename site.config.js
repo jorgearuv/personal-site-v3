bebe70620a54b9fa93ad6150084d57a58638da20
Jorge Ruvalcaba <jorgeruvalcabav@gmail.com>
2022-02-16 10:39:03 -0600

    fix: changing pagesocial type

--- site.config.js
+++ site.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
   rootNotionPageId: 'cce3a9ef1daa4924b0e6f02efea91121',
+  rootNotionSpaceId: 'b7bfc976-dd87-40af-9497-44867776c104',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
